199c0d52e313f77eadc101b8f825616025221c0e
Devin <991476256@qq.com>
2020-08-02 15:34:15 +0800

    [employee][devin] refactor: Merge the same type of exception handling

--- src/main/java/com/thoughtworks/springbootemployee/config/GlobalExceptionHandler.java
+++ src/main/java/com/thoughtworks/springbootemployee/config/GlobalExceptionHandler.java
@@ -1,34 +1,28 @@
 package com.thoughtworks.springbootemployee.config;
 
-import com.thoughtworks.springbootemployee.Exception.*;
-import com.thoughtworks.springbootemployee.constant.ExceptionMessage;
+import com.thoughtworks.springbootemployee.Exception.IllegalUpdateCompanyException;
+import com.thoughtworks.springbootemployee.Exception.IllegalUpdateEmployeeException;
+import com.thoughtworks.springbootemployee.Exception.NoSuchCompanyException;
+import com.thoughtworks.springbootemployee.Exception.NoSuchEmployeeException;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
-    @ExceptionHandler(NoSuchEmployeeException.class)
+    @ExceptionHandler({NoSuchEmployeeException.class, NoSuchCompanyException.class})
     @ResponseStatus(HttpStatus.NOT_FOUND)
-    String handleNoSuchDataException() {
-        return ExceptionMessage.NO_SUCH_EMPLOYEE.getErrorMsg();
+    String handleNoSuchDataException(Exception e) {
+        return e.getMessage();
     }
 
-    @ExceptionHandler(NoSuchCompanyException.class)
+
+    @ExceptionHandler({IllegalUpdateEmployeeException.class, IllegalUpdateCompanyException.class})
     @ResponseStatus(HttpStatus.FORBIDDEN)
-    String handleIllegalOperationException() {
-        return ExceptionMessage.NO_SUCH_COMPANY.getErrorMsg();
+    String handleIllegalOperationException(Exception e) {
+        return e.getMessage();
     }
 
-    @ExceptionHandler(IllegalUpdateEmployeeException.class)
-    @ResponseStatus(HttpStatus.FORBIDDEN)
-    String handleIllegalUpdateEmployeeException() {
-        return ExceptionMessage.ILLEGAL_UPDATE_EMPLOYEE.getErrorMsg();
-    }
-
-    @ExceptionHandler(IllegalUpdateCompanyException.class)
-    @ResponseStatus(HttpStatus.FORBIDDEN)
-    String handleIllegalUpdateCompanyException() {
-        return ExceptionMessage.ILLEGAL_UPDATE_COMPANY.getErrorMsg();
-    }
 }
